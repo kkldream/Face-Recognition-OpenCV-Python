54485d5c34b09faa39e95be675036dd1be667217
kkldream <kk69347321@gmail.com>
2021-09-06 20:32:00 +0800

    Finish face_correction.py

--- mediapipe/lib/header.py
+++ mediapipe/lib/header.py
@@ -1,5 +1,6 @@
 from math import pow, sqrt
 
 def get_distance(point_0, point_1):
-    distance = sqrt(pow((point_0[0] - point_1[0]),2) + pow((point_0[1] - point_1[1]),2))
+    distance = pow((point_0[0] - point_1[0]), 2) + pow((point_0[1] - point_1[1]), 2)
+    distance = sqrt(distance)
     return distance
