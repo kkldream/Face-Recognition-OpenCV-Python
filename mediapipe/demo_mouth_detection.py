1c1c79246c34a415fc7b7b746dbe24ec2c12a5ce
kkldream <kk69347321@gmail.com>
2022-02-09 20:04:17 +0800

    update

--- mediapipe/demo_mouth_detection.py
+++ mediapipe/demo_mouth_detection.py
@@ -6,11 +6,12 @@ from face_mesh import FaceMesh
 from mouth_detection import MouthDetection
 import json
 from keras.models import load_model
-my_model = load_model('model.h5')
+# my_model = load_model('model.h5')
+my_model = load_model('model-67_epoch-0.00268_loss.hdf5')
 
 def main():
 	level = 0
-	percent_list = np.zeros((100, 1))
+	TestList = np.zeros((300, 4))
 	''' CaptureInput '''
 	cap = utils.CaptureInput(0, 640, 480, 30)
 	# cap = utils.CaptureInput('blink_test.mp4', 960, 540)
@@ -19,6 +20,7 @@ def main():
 	face_mesh = FaceMesh(1, 0.7, 0.7)
 	mouth = MouthDetection()
 	cvFpsCalc = utils.CvFpsCalc(buffer_len=10)
+	last_rotation = 0, 0, 0
 	''' Start Loop'''
 	while True:
 		display_fps = cvFpsCalc.get()
@@ -26,17 +28,20 @@ def main():
 		face_results = face_mesh(frame)
 		for face_result in face_results:
 			''' mouth detection '''
-			percent = mouth(face_result, display_fps)
-			mouth.draw_mouth_edge(frame, face_result)
-			cv2.putText(frame, f'{int(percent)}%', face_result[81][:2],
+			mouth_ear = calc_mouth_ear(face_result)
+			rotation = face_mesh.get_rotation(face_result)
+			frame = draw_mouth_edge(frame.copy(), face_result)
+			Test = np.array([
+				map(mouth_ear, 0, 1, 0, 1),
+				map(rotation[0] - last_rotation[0], -50, 50, -1, 1),
+				map(rotation[1] - last_rotation[1], -50, 50, -1, 1),
+				map(rotation[2] - last_rotation[2], -50, 50, -1, 1)
+			])
+			cv2.putText(frame, f'{int(mouth_ear*100)}%', face_result[81][:2],
 						cv2.FONT_HERSHEY_SIMPLEX, 0.6, (0, 0, 255), 2)
-			percent_list[:-1] = percent_list[1:]
-			percent_list[-1] = percent
-			train_mean = (1.809302, 29.645587)
-			train_max = (5.000000, 85.310071)
-			train_min = (0.00000, 2.04622)
-			x = (percent_list - train_mean[1]) / (train_max[1] - train_min[1])
-			predicted = my_model.predict(np.array([x]))
+			TestList[:-1] = TestList[1:]
+			TestList[-1] = Test
+			predicted = my_model.predict(np.array([TestList]))
 			level = max(0, min(5, int(predicted[0,0] * 10)))
 			# print(level)
 
@@ -45,13 +50,14 @@ def main():
 			# 	'mouth' : percent
 			# }
 			# fileAppendTextLine(json.dumps(msg_dict))
+		last_rotation = rotation
 		''' display '''
 		cv2.putText(frame, "FPS:" + str(display_fps), (10, 30),
 					cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 0), 2)
 		cv2.putText(frame, "Level:" + str(level), (10, 60),
 					cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 0), 2)
 		cv2.imshow('frame', frame)
-		key = cv2.waitKey(100)
+		key = cv2.waitKey(1)
 		if key == 27: # ESC
 			break
 		for i in range(10):
@@ -64,6 +70,31 @@ def main():
 def fileAppendTextLine(msg):
     with open('output2.txt', 'a', encoding='utf-8') as file:
         file.write(f'{msg}\n')
+
+def calc_mouth_ear(landmarks):
+	p1 = landmarks[61]
+	p2 = landmarks[81]
+	p3 = landmarks[311]
+	p4 = landmarks[291]
+	p5 = landmarks[402]
+	p6 = landmarks[178]
+	ear = (utils.get_distance(p2, p6) + utils.get_distance(p3, p5))\
+			/ (2 * utils.get_distance(p1, p4))
+	return ear
+
+def draw_mouth_edge(image, landmarks):
+	pos = (61, 81, 311, 291, 402, 178)
+	for p in pos:
+		cv2.circle(image, landmarks[p][:2], 2, (0, 0, 255), -1)
+	return image
+
+def map(var, in_min, in_max, out_min, out_max):
+    var = float(var)
+    if var >= in_max:
+        return out_max
+    if var <= in_min:
+        return out_min
+    return (var - in_min) * (out_max - out_min) / (in_max - in_min) + out_min
 
 if __name__ == '__main__':
 	main()
